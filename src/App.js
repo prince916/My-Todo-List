3ee7e28e379388eb812c3c3482bfcc2ef22463c7
Your Name <you@example.com>
2023-01-02 23:12:44 +0530

    next commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from "./logo.svg";
+
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const AddElement = (event) => {
+    setInput(event.target.value);
+  };
+
   return (
     <>
       <div className="Container">
         <div className="wrapper">
           <h1>Today's Todo List</h1>
           <div className="box">
-            <input placeholder="Type here...."></input>
-            <button>+</button>
+            <input type="text" placeholder="Type here...."></input>
+            <button onClick={AddElement}>+</button>
           </div>
         </div>
       </div>
